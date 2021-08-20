b8b1f0995068cf4143861981f1b48babe1c97881
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2021-08-20 15:46:34 +0900

    カードアニメーション追加

--- handDeck.py
+++ handDeck.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+import threading
+import time
 
 class HandDeck():
 
 	def __init__(self, imagewidth, imageheight, gwidth, gheight):
 		self.cards = []
 		self.tosscards = []
+		self.cardPressed = False
 		self.drawcount = 0
 		self.imagewidth = imagewidth
 		self.imageheight = imageheight
@@ -35,13 +38,48 @@ class HandDeck():
 			self.cards.pop(0)
 			self.addDrawCount()
 
+		self.cardPressed = True
+
 	def getCards(self):
 		return self.cards
 
 	def getTossCards(self):
 		return self.tosscards
 
+	def handCardAnim(self, canvas):
+		if self.cards[0].getState() == 2:
+			return
+
+		canvas.create_image(self.handcardsX, self.Y, image = self.cards[0].getImage(), anchor = tk.NW, tags = self.cards[0].getTags())
+		splitcount = 5
+		x = (self.handcardsX - self.drawcardX) / splitcount
+		for i in range(splitcount):
+			canvas.move(self.cards[0].getTags(), -x, 0)
+			time.sleep(0.1)
+
+	def tossCardAnim(self, canvas):
+		if self.tosscards[-1].getState() == 2:
+			return
+
+		canvas.create_image(self.drawcardX, self.Y, image = self.tosscards[-1].getImage(), anchor = tk.NW, tags = self.tosscards[-1].getTags())
+		splitcount = 5
+		x = (self.drawcardX - self.tossX) / splitcount
+		for i in range(splitcount):
+			canvas.move(self.tosscards[-1].getTags(), -x, 0)
+			time.sleep(0.1)
+
 	def paint(self, canvas, bgcolor, hidetags):
+		handcardthread = threading.Thread(target=self.handCardAnim, args=(canvas,))
+		tosscardthread = threading.Thread(target=self.tossCardAnim, args=(canvas,))
+		handcardthread.setDaemon(True)
+		tosscardthread.setDaemon(True)
+
+		if self.cardPressed == True and len(self.cards) > 0 and handcardthread.is_alive() == False:
+			handcardthread.start()
+
+		if self.cardPressed == True and self.getDrawCount() > 1 and tosscardthread.is_alive() == False:
+			tosscardthread.start()
+
 		# 残りのカード
 		if len(self.cards) > 1:
 			canvas.create_rectangle(self.handcardsX, self.Y, self.handcardsX + self.imagewidth, self.Y + self.imageheight, fill = 'firebrick', outline = 'black', width = 1, tags = 'handcards')
@@ -52,7 +90,8 @@ class HandDeck():
 		else:
 			if self.getDrawCount() > 0:
 				if len(self.cards) > 0:
-					canvas.create_image(self.drawcardX, self.Y, image = self.cards[0].getImage(), anchor = tk.NW, tags = self.cards[0].getTags())
+					if handcardthread.is_alive() == False:
+						canvas.create_image(self.drawcardX, self.Y, image = self.cards[0].getImage(), anchor = tk.NW, tags = self.cards[0].getTags())
 					canvas.create_line(self.drawcardX, self.Y, self.drawcardX, self.Y + self.imageheight, fill = bgcolor, tags = self.cards[0].getTags() + 'line')
 					canvas.create_line(self.drawcardX, self.Y, self.drawcardX + self.imagewidth, self.Y, fill = bgcolor, tags = self.cards[0].getTags() + 'line')
 					canvas.create_line(self.drawcardX + self.imagewidth, self.Y, self.drawcardX + self.imagewidth, self.Y + self.imageheight, fill = bgcolor, tags = self.cards[0].getTags() + 'line')
@@ -65,8 +104,13 @@ class HandDeck():
 				self.tosscards.pop(-1)
 
 			if len(self.tosscards) > 0:
-				canvas.create_image(self.tossX, self.Y, image = self.tosscards[-1].getImage(), anchor = tk.NW, tags = self.tosscards[-1].getTags())
+				if len(self.tosscards) > 1:
+					canvas.create_image(self.tossX, self.Y, image = self.tosscards[-2].getImage(), anchor = tk.NW, tags = self.tosscards[-2].getTags())
+				if tosscardthread.is_alive() == False:
+					canvas.create_image(self.tossX, self.Y, image = self.tosscards[-1].getImage(), anchor = tk.NW, tags = self.tosscards[-1].getTags())
 				canvas.create_line(self.tossX, self.Y, self.tossX, self.Y + self.imageheight, fill = bgcolor, tags = self.tosscards[-1].getTags() + 'line')
 				canvas.create_line(self.tossX, self.Y, self.tossX + self.imagewidth, self.Y, fill = bgcolor, tags = self.tosscards[-1].getTags() + 'line')
 				canvas.create_line(self.tossX + self.imagewidth, self.Y, self.tossX + self.imagewidth, self.Y + self.imageheight, fill = bgcolor, tags = self.tosscards[-1].getTags() + 'line')
 				canvas.create_line(self.tossX, self.Y + self.imageheight, self.tossX + self.imagewidth, self.Y + self.imageheight, fill = bgcolor, tags = self.tosscards[-1].getTags() + 'line')
+
+		self.cardPressed = False
